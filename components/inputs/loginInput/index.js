5a6c6ae6b85523a95bc4e52311001d4e3733de80
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-03-30 16:45:35 +0530

    providers setup

--- components/inputs/loginInput/index.js
+++ components/inputs/loginInput/index.js
@@ -2,10 +2,16 @@ import styles from "./styles.module.scss";
 import { BiUser } from "react-icons/bi";
 import { SiMinutemailer } from "react-icons/si";
 import { IoKeyOutline } from "react-icons/io5";
+import { useField, ErrorMessage } from "formik";
 
-export default function LoginInput({ icon, placeholder }) {
+export default function LoginInput({ icon, placeholder, ...props }) {
+  const [field, meta] = useField(props);
   return (
-    <div className={styles.input}>
+    <div
+      className={`${styles.input} ${
+        meta.touched && meta.error ? styles.error : ""
+      }`}
+    >
       {icon == "user" ? (
         <BiUser />
       ) : icon == "email" ? (
@@ -15,7 +21,19 @@ export default function LoginInput({ icon, placeholder }) {
       ) : (
         ""
       )}
-      <input type="text" placeholder={placeholder} />
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error && (
+        <div className={styles.error__popup}>
+          <span></span>
+          <ErrorMessage name={field.name} />
+        </div>
+      )}
     </div>
   );
 }
--- pages/signin.js
+++ pages/signin.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import LoginInput from "@/components/inputs/loginInput";
 import styles from "@/styles/signin.module.scss";
 import { Formik, Form } from "formik";
+import * as Yup from "yup";
 import Link from "next/link";
 import { BiLeftArrowAlt } from "react-icons/bi";
+import CircledIconBtn from "@/components/buttons/circledIconBtn";
+import { getProviders } from "next-auth/react";
 
-export default function signin() {
+const initialvalues = {
+  email: "",
+  password: "",
+};
+
+export default function Signin(providers) {
+  const [user, setUser] = useState(initialvalues);
+  const { login_email, login_password } = user;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email Address is required")
+      .email("Please enter a valid email address"),
+    login_password: Yup.string().required("Please enter your password"),
+  });
+
   return (
     <>
       <Header />
@@ -25,20 +47,46 @@ export default function signin() {
             <p>
               Get access to one of the best EShopping services in the world.
             </p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              initialValues={{ login_email, login_password }}
+              validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form>
                   <LoginInput
-                    icon="password"
-                    placeholder="Enter your password"
+                    type="text"
+                    name="login_email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
                   />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <CircledIconBtn type="submit" text="Sign in" />
+                  <div className={styles.forgot}>
+                    <Link href="/forget">Forgot Password ?</Link>
+                  </div>
                 </Form>
               )}
             </Formik>
+           
           </div>
         </div>
       </div>
       <Footer country="India" />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const providers = Object.values(await getProviders());
+  return {
+    props: { providers },
+  };
 }
