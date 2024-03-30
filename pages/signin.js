a4f36e7845656afa0e01c18e79a6a68e963d6348
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-03-31 02:00:38 +0530

    sign up page

--- pages/signin.js
+++ pages/signin.js
@@ -9,15 +9,27 @@ import Link from "next/link";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import CircledIconBtn from "@/components/buttons/circledIconBtn";
 import { getProviders } from "next-auth/react";
+import { signIn } from "next-auth/react";
 
 const initialvalues = {
+  login_email: "",
+  login_password: "",
+  name: "",
   email: "",
   password: "",
+  confirm_password: "",
 };
 
 export default function Signin(providers) {
   const [user, setUser] = useState(initialvalues);
-  const { login_email, login_password } = user;
+  const {
+    login_email,
+    login_password,
+    name,
+    email,
+    password,
+    confirm_password,
+  } = user;
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
@@ -28,7 +40,27 @@ export default function Signin(providers) {
       .email("Please enter a valid email address"),
     login_password: Yup.string().required("Please enter your password"),
   });
-
+  const registerValidation = Yup.object({
+    name: Yup.string()
+      .required("What's your name ?")
+      .min(2, "First name must be between 2 and 16 characters.")
+      .max(16, "First name must be between 2 and 16 characters.")
+      .matches(/^[aA-zZ]/, "Numbers and special characters are not allowed."),
+    email: Yup.string()
+      .required(
+        "You'll need this when you log in and if you ever need to reset your password."
+      )
+      .email("Enter a valid email address."),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &)."
+      )
+      .min(6, "Password must be atleast 6 characters.")
+      .max(36, "Password can't be more than 36 characters"),
+    confirm_password: Yup.string()
+      .required("Confirm your password.")
+      .oneOf([Yup.ref("password")], "Passwords must match."),
+  });
   return (
     <>
       <Header />
@@ -84,12 +116,60 @@ export default function Signin(providers) {
                       className={styles.social__btn}
                       onClick={() => signIn(provider.id)}
                     >
+                      <img src={`../../icons/${provider.name}.png`} alt="" />
                       Sign in with {provider.name}
                     </button>
                   </div>
                 ))}
               </div>
             </div>
+          </div>
+        </div>
+        <div className={styles.login__container}>
+          <div className={styles.login__form}>
+            <h1>Sign up</h1>
+            <p>
+              Get access to one of the best EShopping services in the world.
+            </p>
+            <Formik
+              enableReinitialize
+              initialValues={{ name, email, password, confirm_password }}
+              validationSchema={registerValidation}
+            >
+              {(form) => (
+                <Form>
+                  <LoginInput
+                    type="text"
+                    name="name"
+                    icon="user"
+                    placeholder="Full Name"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="text"
+                    name="email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="confirm_password"
+                    icon="password"
+                    placeholder="Re-Type Password"
+                    onChange={handleChange}
+                  />
+                  <CircledIconBtn type="submit" text="Sign up" />
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
