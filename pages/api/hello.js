723e36a25b0560d6726ddbcd74a988afc7c42527
ariz565 <mohdariz.phy@gmail.com>
2024-05-07 03:45:02 +0530

    Added <Readme>: For API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,5 +4,5 @@ import db from "../../utils/db";
 export default function handler(req, res) {
   db.connectDb(); // Call as object property
   db.disconnectDb(); // Call as object property
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "Swift Cart" });
 }
