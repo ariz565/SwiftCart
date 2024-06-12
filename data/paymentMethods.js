b9c1d8361b0e398835c11767fc7011cfa48809ed
ariz565 <mohdariz.phy@gmail.com>
2024-06-12 18:51:32 +0530

    fixed few issues

---
+++ data/paymentMethods.js
@@ -0,0 +1,29 @@
+export const paymentMethods = [
+  {
+    name: "Razorpay",
+    id: "razorpay",
+    description: "Pay securely using Razorpay.",
+    images: ["razorpay"],
+  },
+  {
+    name: "Credit Card",
+    id: "credit_card",
+    description: "",
+    images: [
+      "visa",
+      "mastercard",
+      "paypal",
+      "maestro",
+      "american_express",
+      "cb",
+      "jcb",
+    ],
+  },
+  {
+    name: "Cash",
+    id: "cash",
+    description:
+      "If you don't have a paypal account,you can also pay via paypal with your credit card or bank debit card. Payment can be submitted in any currency!",
+    images: [],
+  },
+];
